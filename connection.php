d6b7eb30fa08119a116f9d7aabf12d62455576a7
SlavaM1 <90597025+SlavaM1@users.noreply.github.com>
2021-09-13 14:05:39 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 $db_host = "localhost"; // ������
-$db_user = "user211"; // ����� ��
-$db_password = "gun_centos_user_211"; // ������ ��
-$db_base = 'user211'; // ��� ��
+$db_user = "root"; // ����� ��
+$db_password = ""; // ������ ��
+$db_base = 'user1'; 
